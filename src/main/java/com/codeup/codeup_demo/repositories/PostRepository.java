5c6720353530aa48650ce8ff28de0c3774883595
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-04-05 11:57:27 -0500

    completed test for CRUD functionality

--- src/main/java/com/codeup/codeup_demo/repositories/PostRepository.java
+++ src/main/java/com/codeup/codeup_demo/repositories/PostRepository.java
@@ -3,14 +3,19 @@ package com.codeup.codeup_demo.repositories;
 import com.codeup.codeup_demo.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-//    List<Post> findByTitle(String title);
+    List<Post> findByTitle(String title);
 //    List<Post> findPostByTitleIsContainingOrBodyContaining(String term1, String term2);
 //    List<Post> findPostByTitleIsContaining(String term);
 //    List<Post> findPostsByBodyContainingOrTitleContaining(String searchParam);
     List<Post> findByBodyIsLikeOrTitleIsLike(String term, String term2);
+
+    @Query("from Post post where post.body like %:term%")
+    List<Post> searchByBodyLike(@Param("term") String term);
+
 }
